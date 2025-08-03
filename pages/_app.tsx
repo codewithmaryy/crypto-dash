5de932e11194a27eb1438aea2327cb2fd20d2cd5
Maryam Shafqat <maryamshafqat17@example.com>
2025-08-04 01:37:44 +0500

    Fix: correct casing for Header.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/globals.css'
-import Header from '../components/header'
+import Header from '../components/Header'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
